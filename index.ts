7d11de32a125d8b8f58625230e0a7125ed5582dc
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-06-24 02:34:10 +0600

    update all tasks

--- index.ts
+++ index.ts
@@ -1,3 +1,67 @@
-const message:string = "Hello TypeScript";
-console.log(`Great, ${message}`);
+// primitive data types
 
+let name: string = "Shahriar";
+let age: number = 30;
+let isDeveloper: boolean = true;
+
+// array in TypeScript
+let numbers: number[] = [1, 2, 3];
+let numbers2: Array<number> = [1, 2, 3];
+
+
+// object type in TypeScript
+
+let user: {
+  name: string;
+  age: number;
+} = {
+  name: "Shahriar",
+  age: 30,
+};
+
+// parameter and return types in a function
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+
+// optional property in TypeScript
+
+type User = {
+  name: string;
+  email?: string;
+};
+
+// readonly property
+
+type Product = {
+  readonly id: number;
+  title: string;
+};
+
+// Type and Interface
+
+type User3 = {
+  name: string;
+};
+
+interface User4 {
+  name: string;
+}
+
+// any
+
+let value: any = 10;
+
+value = "hello";
+value = true;
+
+// Generics
+
+function identity<T>(value: T): T {
+  return value;
+}
+
+identity<string>("Hello");
+identity<number>(100);
